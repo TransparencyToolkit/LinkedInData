641cfa0b3b623f650312ddef7aa0ea79646ea648
Shidash <shidash@shidash.com>
2014-01-07 04:36:54 -0800

    First commit

---
+++ lib/linkedindata.rb
@@ -0,0 +1,69 @@
+require 'mechanize'
+require 'linkedin-scraper'
+require 'json'
+
+class LinkedinData
+  def initialize(input)
+   @input = input
+   @output = Array.new
+  end
+
+  # Searches for links on Google
+  def search
+    agent = Mechanize.new
+    gform = agent.get("http://google.com").form("f")
+    gform.q = "site:linkedin.com/pub " + @input
+    page = agent.submit(gform, gform.buttons.first)
+    examine(page)
+  end
+
+  # Examines a search page
+  def examine(page)
+    startindex = 0
+
+    page.links.each do |link|
+      if (link.href.include? "linkedin.com") && (!link.href.include? "webcache") && (!link.href.include? "site:linkedin.com/pub+")
+        saveurl = link.href.split("?q=")
+        
+        if saveurl[1]
+          url = saveurl[1].split("&")
+          scrape(url[0])
+        end
+      end
+
+      if (link.href.include? "&sa=N") && (link.href.include? "&start=")
+        url1 = link.href.split("&start=")
+        url2 = url1[1].split("&sa=N")
+
+        if url2[0].to_i < startindex
+          agent = Mechanize.new
+          examine(agent.get("http://google.com" + link.href))
+        else startindex = url2[0].to_i
+        end
+      end
+    end
+  end
+
+  # Scrapes profile and makes JSON
+  def scrape(url)
+    profile = Linkedin::Profile.get_profile(url)
+    
+    if profile
+      profile.current_companies.each do |c|
+        c.merge!(:name => profile.first_name + " " + profile.last_name)
+        @output.push(c)
+      end
+      
+      profile.past_companies.each do |c|
+        c.merge!(:name => profile.first_name + " " + profile.last_name)
+        @output.push(c)
+      end
+    end
+  end
+
+  # Gets all data and returns in JSON
+  def getData
+    search
+    return @output.to_json
+  end
+end
---
+++ linkedindata.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'linkedindata'
+  s.version     = '0.0.1'
+  s.date        = '2014-01-07'
+  s.summary     = 'Get all LinkedIn profiles including terms you specify'
+  s.description = 'Scrapes all LinkedIn profiles including terms you specify.'
+  s.authors     = ['M. C. McGrath']
+  s.email       = 'shidash@shidash.com'
+  s.files       = ["lib/linkedindata.rb"]
+  s.homepage    =
+    'https://github.com/Shidash/LinkedInData'
+  s.license       = 'GPL'
+end
