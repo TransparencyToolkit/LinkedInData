eda322d7d1570613aa41784e78bbadc7487cef0c
GH <shidash@shidash.com>
2015-04-10 18:59:29 +0200

    Use generalscraper for Google scraping part

--- lib/linkedindata.rb
+++ lib/linkedindata.rb
@@ -1,5 +1,6 @@
 require 'mechanize'
 require 'linkedin-scraper'
+require 'generalscraper'
 require 'json'
 require 'nokogiri'
 require 'open-uri'
@@ -9,56 +10,25 @@ require 'urlarchiver'
 require 'set'
 
 class LinkedinData
-  def initialize(input, todegree)
-    @input = input
+  def initialize(searchterms, todegree, proxy_list)
+    @searchterms = searchterms
+    @proxy_list = proxy_list
     @output = Array.new
     @startindex = 10
     @numhops = todegree
   end
 
+  # TODO:
+  # Make it possible to just get one profile plus degrees out
+  # Change parser
+  # Refactor
+  # Readme and gems
+
   # Searches for profiles on Google
   def search
-    agent = Mechanize.new
-    agent.user_agent_alias = 'Linux Firefox'
-    gform = agent.get("http://google.com").form("f")
-    gform.q = "site:linkedin.com/pub " + @input
-    page = agent.submit(gform, gform.buttons.first)
-    examine(page)
-  end
- 
-  # Examines a search page
-  def examine(page)
-    # Separate getting profile links and going to next page
-      # Method for getting links to all result pages
-      # Different method for getting all profile links on page and scraping (split to new thread for this)
-         # Has own output set, merge into full one at end (make sure threadsafe)
-      
-    # Have own input and output
-    page.links.each do |link|
-      if (link.href.include? "linkedin.com") && (!link.href.include? "webcache") && (!link.href.include? "site:linkedin.com/pub+")
-        saveurl = link.href.split("?q=")
-        
-        if saveurl[1]
-          url = saveurl[1].split("&")
-          begin
-            scrape(url[0], 0)
-          rescue
-          end
-        end
-      end
-
-      # Find the link to the next page and go to it
-      if (link.href.include? "&sa=N") && (link.href.include? "&start=")
-        url1 = link.href.split("&start=")
-        url2 = url1[1].split("&sa=N")
-
-        if url2[0].to_i == @startindex
-          sleep(rand(30..90))
-          @startindex += 10
-          agent = Mechanize.new
-          examine(agent.get("http://google.com" + link.href))
-        end
-      end
+    g = GeneralScraper.new("site:linkedin.com/pub", @searchterms, @proxy_list)
+    JSON.parse(g.getURLs).each do |profile|
+      scrape(profile, @numhops)
     end
   end
 
@@ -157,3 +127,5 @@ class LinkedinData
   end
 end
 
+l = LinkedinData.new("xkeyscore SIGINT", 0, "../../newproxylist")
+puts l.getData
